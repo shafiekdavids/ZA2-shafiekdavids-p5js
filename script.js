a1889e65a1ee72ca6f16164c1239fb868d69e009
shafiekdavids <102852044+shafiekdavids@users.noreply.github.com>
2022-06-18 14:33:09 +0200

    color change
    change color from ##52728C to #0f25d1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //An array of nice color codes we shall pick from
-let niceColors = ["#C5514C", "#EC9D6C", "#FAE2B7", "#6D7D8F", "#52728C"];
+let niceColors = ["#C5514C", "#EC9D6C", "#FAE2B7", "#6D7D8F", "#0f25d1"];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
